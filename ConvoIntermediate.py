213c4869d3221a40767c9717bac5f5e46469da7c
Daniel Alfaro R <daniel.alfaro@upr.edu>
2019-12-05 11:46:13 -0400

    Started communicating parser and intermediate

--- ConvoIntermediate.py
+++ ConvoIntermediate.py
@@ -1,2 +1,14 @@
 def printer():
     print("Welcome to the intermediate code!")
+
+def openComm(p):
+    print("Opening Communication with: ")
+
+def closeComm():
+    print("Closing Communication with: ")
+
+def sendData():
+    print("Sending Data to: ")
+
+def ping():
+    print("Ping to: ")
